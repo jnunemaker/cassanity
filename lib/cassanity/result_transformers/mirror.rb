ddf5f39c4f187ed059f099945edd3ed63882cf97
John Nunemaker <nunemaker@gmail.com>
2012-11-06 14:25:37 -0500

    Added mirror result transformer.
    Just returns whatever is sent. To be used as default result transformer.

---
+++ lib/cassanity/result_transformers/mirror.rb
@@ -0,0 +1,9 @@
+module Cassanity
+  module ResultTransformers
+    class Mirror
+      def call(result)
+        result
+      end
+    end
+  end
+end
---
+++ spec/unit/cassanity/result_transformers/mirror_spec.rb
@@ -0,0 +1,12 @@
+require 'helper'
+require 'cassanity/result_transformers/mirror'
+
+describe Cassanity::ResultTransformers::Mirror do
+  describe "#call" do
+    it "returns whatever is passed it" do
+      [1, '2', ['something'], {something: 'else'}].each do |result|
+        subject.call(result).should be(result)
+      end
+    end
+  end
+end
