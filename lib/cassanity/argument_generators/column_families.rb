3c4a623daefd6e8033f9c3114ca507583c2e18d0
John Nunemaker <nunemaker@gmail.com>
2012-12-10 16:49:03 -0500

    Added Column Families argument generator.

---
+++ lib/cassanity/argument_generators/column_families.rb
@@ -0,0 +1,19 @@
+module Cassanity
+  module ArgumentGenerators
+    class ColumnFamilies
+
+      # Internal
+      def call(args = {})
+        variables = []
+        cql = 'SELECT * FROM system.schema_columnfamilies'
+
+        if (keyspace_name = args[:keyspace_name])
+          cql << ' WHERE "keyspace" = ?'
+          variables << keyspace_name
+        end
+
+        [cql, *variables]
+      end
+    end
+  end
+end
---
+++ spec/unit/cassanity/argument_generators/column_families_spec.rb
@@ -0,0 +1,23 @@
+require 'helper'
+require 'cassanity/argument_generators/column_families'
+
+describe Cassanity::ArgumentGenerators::ColumnFamilies do
+  describe "#call" do
+    context "without keyspace" do
+      it "returns array of arguments for selecting all column families" do
+        cql = 'SELECT * FROM system.schema_columnfamilies'
+        expected = [cql]
+        subject.call.should eq(expected)
+      end
+    end
+
+    context "with :keyspace_name" do
+      it "returns array of arguments for selecting all column families for keyspace" do
+        cql = 'SELECT * FROM system.schema_columnfamilies WHERE "keyspace" = ?'
+        variables = ['foo']
+        expected = [cql, 'foo']
+        subject.call(keyspace_name: 'foo').should eq(expected)
+      end
+    end
+  end
+end
