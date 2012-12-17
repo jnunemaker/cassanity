77b9db9900a0f8fa62088aa1ff2b020f1f131e86
John Nunemaker <nunemaker@gmail.com>
2012-12-17 17:15:56 -0500

    Added top level cassanity shortcut methods for operators and counter update values.

--- lib/cassanity.rb
+++ lib/cassanity.rb
@@ -1,4 +1,88 @@
+require 'cassanity/operators/eq'
+require 'cassanity/operators/gt'
+require 'cassanity/operators/gte'
+require 'cassanity/operators/lt'
+require 'cassanity/operators/lte'
+
 module Cassanity
+  # Public: Shortcut for returning an equality operator.
+  #
+  # args - The arguments to pass to the initialize method of the operator.
+  #
+  # Returns a Cassanity::Operators::Eq instance.
+  def self.eq(*args)
+    Operators::Eq.new(*args)
+  end
+
+  # Public: Shortcut for returning a less than operator.
+  #
+  # args - The arguments to pass to the initialize method of the operator.
+  #
+  # Returns a Cassanity::Operators::Lt instance.
+  def self.lt(*args)
+    Operators::Lt.new(*args)
+  end
+
+  # Public: Shortcut for returning a less than or equal to operator.
+  #
+  # args - The arguments to pass to the initialize method of the operator.
+  #
+  # Returns a Cassanity::Operators::Lte instance.
+  def self.lte(*args)
+    Operators::Lte.new(*args)
+  end
+
+  # Public: Shortcut for returning a greater than operator.
+  #
+  # args - The arguments to pass to the initialize method of the operator.
+  #
+  # Returns a Cassanity::Operators::Gt instance.
+  def self.gt(*args)
+    Operators::Gt.new(*args)
+  end
+
+  # Public: Shortcut for returning a greater than or equal to operator.
+  #
+  # args - The arguments to pass to the initialize method of the operator.
+  #
+  # Returns a Cassanity::Operators::Gte instance.
+  def self.gte(*args)
+    Operators::Gte.new(*args)
+  end
+
+  # Public: Shortcut for returning an increment value for a counter update.
+  #
+  # value - The value to initialize the increment with (optional, default: 1).
+  #
+  # Returns a Cassanity::Increment instance.
+  def self.inc(value = 1)
+    Increment.new(value)
+  end
+
+  # Public: Shortcut for returning a decrement value for a counter update.
+  #
+  # value - The value to initialize the decrement with (optional, default: 1).
+  #
+  # Returns a Cassanity::Decrement instance.
+  def self.dec(value = 1)
+    Decrement.new(value)
+  end
+
+  class << self
+    alias_method :equal, :eq
+
+    alias_method :greater_than, :gt
+    alias_method :greater_than_or_equal_to, :gte
+
+    alias_method :less_than, :lt
+    alias_method :less_than_or_equal_to, :lte
+
+    alias_method :incr, :inc
+    alias_method :increment, :inc
+
+    alias_method :decr, :dec
+    alias_method :decrement, :dec
+  end
 end
 
 require 'cassanity/connection'
--- spec/unit/cassanity_spec.rb
+++ spec/unit/cassanity_spec.rb
@@ -1,5 +1,123 @@
 require 'helper'
 
 describe Cassanity do
+  describe ".eq" do
+    it "returns equality operator" do
+      Cassanity.eq(5).should eq(Cassanity::Operators::Eq.new(5))
+    end
+  end
 
+  describe ".equal" do
+    it "returns equality operator" do
+      Cassanity.equal(5).should eq(Cassanity::Operators::Eq.new(5))
+    end
+  end
+
+  describe ".gt" do
+    it "returns greater than operator" do
+      Cassanity.gt(5).should eq(Cassanity::Operators::Gt.new(5))
+    end
+  end
+
+  describe ".greater_than" do
+    it "returns greater than operator" do
+      Cassanity.greater_than(5).should eq(Cassanity::Operators::Gt.new(5))
+    end
+  end
+
+  describe ".gte" do
+    it "returns greater than or equal to operator" do
+      Cassanity.gte(5).should eq(Cassanity::Operators::Gte.new(5))
+    end
+  end
+
+  describe ".greater_than_or_equal_to" do
+    it "returns greater than operator" do
+      Cassanity.greater_than_or_equal_to(5).should eq(Cassanity::Operators::Gte.new(5))
+    end
+  end
+
+  describe ".lt" do
+    it "returns less than operator" do
+      Cassanity.lt(5).should eq(Cassanity::Operators::Lt.new(5))
+    end
+  end
+
+  describe ".less_than" do
+    it "returns less than operator" do
+      Cassanity.less_than(5).should eq(Cassanity::Operators::Lt.new(5))
+    end
+  end
+
+  describe ".lte" do
+    it "returns less than or equal to operator" do
+      Cassanity.lte(5).should eq(Cassanity::Operators::Lte.new(5))
+    end
+  end
+
+  describe ".less_than_or_equal_to" do
+    it "returns less than or equal to operator" do
+      Cassanity.less_than_or_equal_to(5).should eq(Cassanity::Operators::Lte.new(5))
+    end
+  end
+
+  describe ".inc" do
+    it "returns increment instance" do
+      Cassanity.inc(5).should eq(Cassanity::Increment.new(5))
+    end
+
+    it "returns increment instance with value of 1" do
+      Cassanity.inc.should eq(Cassanity::Increment.new(1))
+    end
+  end
+
+  describe ".incr" do
+    it "returns increment instance" do
+      Cassanity.incr(5).should eq(Cassanity::Increment.new(5))
+    end
+
+    it "returns increment instance with value of 1" do
+      Cassanity.incr.should eq(Cassanity::Increment.new(1))
+    end
+  end
+
+  describe ".increment" do
+    it "returns increment instance" do
+      Cassanity.increment(5).should eq(Cassanity::Increment.new(5))
+    end
+
+    it "returns increment instance with value of 1" do
+      Cassanity.increment.should eq(Cassanity::Increment.new(1))
+    end
+  end
+
+  describe ".dec" do
+    it "returns decrement instance" do
+      Cassanity.dec(5).should eq(Cassanity::Decrement.new(5))
+    end
+
+    it "returns decrement instance with value of 1" do
+      Cassanity.dec.should eq(Cassanity::Decrement.new(1))
+    end
+  end
+
+  describe ".decr" do
+    it "returns decrement instance" do
+      Cassanity.decr(5).should eq(Cassanity::Decrement.new(5))
+    end
+
+    it "returns decrement instance with value of 1" do
+      Cassanity.decr.should eq(Cassanity::Decrement.new(1))
+    end
+  end
+
+  describe ".decrement" do
+    it "returns decrement instance" do
+      Cassanity.decrement(5).should eq(Cassanity::Decrement.new(5))
+    end
+
+    it "returns decrement instance with value of 1" do
+      Cassanity.decrement.should eq(Cassanity::Decrement.new(1))
+    end
+  end
 end
