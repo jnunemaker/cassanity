343ac0bd08c227ad995eb49fa47666435faff4ed
John Nunemaker <nunemaker@gmail.com>
2012-12-17 16:12:42 -0500

    Require logger for shared examples.

--- examples/_shared.rb
+++ examples/_shared.rb
@@ -2,6 +2,7 @@
 # Sets up load path for examples and requires some stuff
 require 'pp'
 require 'pathname'
+require 'logger'
 
 root_path = Pathname(__FILE__).dirname.join('..').expand_path
 lib_path  = root_path.join('lib')
