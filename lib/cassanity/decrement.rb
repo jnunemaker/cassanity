0cb3fcf945c14a614d6e8649a71f6be23ae69801
John Nunemaker <nunemaker@gmail.com>
2013-01-21 13:29:26 -0500

    Make increment and decrement inherit from operator.

--- lib/cassanity/decrement.rb
+++ lib/cassanity/decrement.rb
@@ -3,25 +3,12 @@ module Cassanity
     Decrement.new(*args)
   end
 
-  class Decrement
-    # Internal
-    attr_reader :symbol
-
-    # Internal
-    attr_reader :value
-
+  class Decrement < Operator
     # Public: Returns an decrement instance
     def initialize(value = 1)
       raise ArgumentError.new("value cannot be nil") if value.nil?
 
-      @symbol = :-
-      @value = value
+      super :-, value
     end
-
-    def eql?(other)
-      self.class.eql?(other.class) && value == other.value
-    end
-
-    alias_method :==, :eql?
   end
 end
--- lib/cassanity/increment.rb
+++ lib/cassanity/increment.rb
@@ -3,25 +3,12 @@ module Cassanity
     Increment.new(*args)
   end
 
-  class Increment
-    # Internal
-    attr_reader :symbol
-
-    # Internal
-    attr_reader :value
-
+  class Increment < Operator
     # Public: Returns an increment instance
     def initialize(value = 1)
       raise ArgumentError.new("value cannot be nil") if value.nil?
 
-      @symbol = :+
-      @value = value
+      super :+, value
     end
-
-    def eql?(other)
-      self.class.eql?(other.class) && value == other.value
-    end
-
-    alias_method :==, :eql?
   end
 end
