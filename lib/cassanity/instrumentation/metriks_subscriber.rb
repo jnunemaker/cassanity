d5ac05ad02ffa8221166ff244e5e0a83ea823fe1
John Nunemaker <nunemaker@gmail.com>
2013-01-25 17:45:50 -0500

    Noted that metriks is only file you need to require.

--- lib/cassanity/instrumentation/metriks_subscriber.rb
+++ lib/cassanity/instrumentation/metriks_subscriber.rb
@@ -1,3 +1,7 @@
+# Note: You should never need to require this file directly if you are using
+# ActiveSupport::Notifications. Instead, you should require the metriks file
+# that lives in the same directory as this file. The benefit is that it
+# subscribes to the correct events and does everything for your.
 require 'metriks'
 
 module Cassanity
