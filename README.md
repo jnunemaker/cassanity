0294c7b617f003ef18c22ecfbf485f12f4d6eae6
John Nunemaker <nunemaker@gmail.com>
2013-02-27 12:19:41 -0500

    Instrumentation readme and spec tweaks.

--- README.md
+++ README.md
@@ -110,22 +110,26 @@ default these work with ActiveSupport::Notifications, but only require the
 pieces of ActiveSupport that are needed and only do so if you actually attempt
 to require the instrumentation files listed below.
 
-If you use any of this instrumentation, you will need to have activesupport in
-your Gemfile.
-
 To use the log subscriber:
 
 ```ruby
+# Gemfile
+gem 'activesupport'
+
+# config/initializers/cassanity.rb (or wherever you want it)
 require 'cassanity/instrumentation/log_subscriber'
 ```
 
 To use the metriks instrumentation:
 
 ```ruby
+# Gemfile
+gem 'activesupport'
+gem 'metriks'
+
+# config/initializers/cassanity.rb (or wherever you want it)
 require 'cassanity/instrumentation/metriks'
 ```
-
-For Rails, I typically drop these lines in an initialize named `cassanity.rb`.
 
 ## Compatibility
 
--- spec/integration/cassanity/instrumentation/log_subscriber_spec.rb
+++ spec/integration/cassanity/instrumentation/log_subscriber_spec.rb
@@ -25,30 +25,29 @@ describe Cassanity::Instrumentation::LogSubscriber do
 
   before do
     @io = StringIO.new
-    logger = Logger.new(@io)
-    Cassanity::Instrumentation::LogSubscriber.logger = logger
+    Cassanity::Instrumentation::LogSubscriber.logger = Logger.new(@io)
 
     keyspace.recreate
     column_family.recreate
   end
 
-  it "works" do
-    begin
-      column_family.insert({
-        data: {
-          id: SimpleUUID::UUID.new,
-          name: 'GitHub.com',
-        },
-      })
+  after do
+    Cassanity::Instrumentation::LogSubscriber.logger = nil
+  end
 
-      query = "INSERT INTO cassanity_test.apps (id, name) VALUES (?, ?)"
-      log = @io.string
-      log.should match(/#{Regexp.escape(query)}/i)
-      log.should match(/UUID/i)
-      log.should match(/GitHub\.com/i)
-    ensure
-      Cassanity::Instrumentation::LogSubscriber.logger = nil
-    end
+  it "works" do
+    column_family.insert({
+      data: {
+        id: SimpleUUID::UUID.new,
+        name: 'GitHub.com',
+      },
+    })
+
+    query = "INSERT INTO cassanity_test.apps (id, name) VALUES (?, ?)"
+    log = @io.string
+    log.should match(/#{Regexp.escape(query)}/i)
+    log.should match(/UUID/i)
+    log.should match(/GitHub\.com/i)
   end
 
   it "does not fail when no bind variables" do
