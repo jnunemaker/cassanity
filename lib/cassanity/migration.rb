14f84b8a0ba6b1647cef0aee6572e1f898ccaa52
John Nunemaker <nunemaker@gmail.com>
2013-02-27 15:45:43 -0500

    Migration equality and initialization from hash or path.

--- lib/cassanity/migration.rb
+++ lib/cassanity/migration.rb
@@ -1,14 +1,27 @@
 module Cassanity
   class Migration
-    # Public: The name of the migration.
-    attr_reader :name
+    def self.from_path(path)
+      basename = File.basename(path, '.rb')
+      version, name = basename.split('_', 2)
+      new(version, name)
+    end
+
+    def self.from_hash(hash)
+      new(hash['version'], hash['name'])
+    end
 
     # Public: The version of the migration.
     attr_reader :version
 
-    def initialize(name, version)
+    # Public: The name of the migration.
+    attr_reader :name
+
+    def initialize(version, name)
+      raise ArgumentError, "version cannot be nil" if version.nil?
+      raise ArgumentError, "name cannot be nil" if name.nil?
+
+      @version = version.to_i
       @name = name
-      @version = version
     end
 
     def up
@@ -18,5 +31,12 @@ module Cassanity
     def down
       # override in subclass
     end
+
+    def eql?(other)
+      self.class.eql?(other.class) &&
+        version == other.version &&
+        name == other.name
+    end
+    alias_method :==, :eql?
   end
 end
--- spec/unit/cassanity/migration_spec.rb
+++ spec/unit/cassanity/migration_spec.rb
@@ -2,27 +2,110 @@ require 'helper'
 require 'cassanity/migration'
 
 describe Cassanity::Migration do
-  describe "#initialize" do
+  describe ".from_path" do
     before do
-      @result = described_class.new('CreateUsers', 1234)
+      @result = described_class.from_path('/some/path/1_foo.rb')
     end
 
-    it "sets name" do
-      @result.name.should eq('CreateUsers')
+    it "returns instance of migration" do
+      @result.should be_instance_of(described_class)
     end
 
     it "sets version" do
-      @result.version.should eq(1234)
+      @result.version.should be(1)
+    end
+
+    it "sets name" do
+      @result.name.should eq('foo')
+    end
+  end
+
+  describe ".from_hash" do
+    before do
+      @result = described_class.from_hash({'version' => '1', 'name' => 'foo'})
+    end
+
+    it "returns instance of migration" do
+      @result.should be_instance_of(described_class)
+    end
+
+    it "sets version" do
+      @result.version.should be(1)
+    end
+
+    it "sets name" do
+      @result.name.should eq('foo')
+    end
+  end
+
+  describe "#initialize" do
+    context "with nil version" do
+      it "raises argument error" do
+        expect {
+          described_class.new(nil, 'foo')
+        }.to raise_error(ArgumentError, 'version cannot be nil')
+      end
+    end
+
+    context "with nil name" do
+      it "raises argument error" do
+        expect {
+          described_class.new(1, nil)
+        }.to raise_error(ArgumentError, 'name cannot be nil')
+      end
+    end
+
+    context "with integer version" do
+      before do
+        @result = described_class.new(1234, 'CreateUsers')
+      end
+
+      it "sets version" do
+        @result.version.should eq(1234)
+      end
+
+      it "sets name" do
+        @result.name.should eq('CreateUsers')
+      end
+    end
+
+    context "with string version" do
+      it "sets version to integer" do
+        migration = described_class.new('1234', 'CreateUsers')
+        migration.version.should be(1234)
+      end
     end
   end
 
   it "responds to up" do
-    instance = described_class.new('CreateUsers', 1234)
+    instance = described_class.new(1234, 'CreateUsers')
     instance.should respond_to(:up)
   end
 
   it "responds to down" do
-    instance = described_class.new('CreateUsers', 1234)
+    instance = described_class.new(1234, 'CreateUsers')
     instance.should respond_to(:down)
+  end
+
+  describe "#eql?" do
+    it "returns true for same class, name, and version" do
+      other = described_class.new(1, 'a')
+      described_class.new(1, 'a').eql?(other).should be_true
+    end
+
+    it "returns false for different name" do
+      other = described_class.new(1, 'b')
+      described_class.new(1, 'a').eql?(other).should be_false
+    end
+
+    it "returns false for different version" do
+      other = described_class.new(2, 'a')
+      described_class.new(1, 'a').eql?(other).should be_false
+    end
+
+    it "returns false for different class" do
+      other = Object.new
+      described_class.new(1, 'a').eql?(other).should be_false
+    end
   end
 end
