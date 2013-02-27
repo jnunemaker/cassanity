7e454c3fad182e1cc917b75a70cf73778f1adcab
John Nunemaker <nunemaker@gmail.com>
2013-02-27 16:03:12 -0500

    Migrating from a directory of migrations works.

---
+++ lib/cassanity/migrator.rb
@@ -0,0 +1,72 @@
+require 'pathname'
+require 'cassanity/migration'
+require 'cassanity/migration/collection'
+
+module Cassanity
+  class Migrator
+    # Public: The keyspace all migrations apply to.
+    attr_reader :keyspace
+
+    # Public: The path to all the migrations.
+    attr_reader :migrations_path
+
+    def initialize(keyspace, migrations_path)
+      @keyspace = keyspace
+      @migrations_path = Pathname(migrations_path)
+    end
+
+    def migrate
+      ensure_column_family_exists
+
+      migrations_from_path.each do |migration|
+        migration.run(self, :up)
+      end
+    end
+
+    # Marks a migration as migrated.
+    def migrated(migration)
+      column_family.insert({
+        data: {
+          version: version,
+          name: name,
+          migrated_at: Time.now.utc,
+        },
+      })
+    end
+
+    def migrations_from_path
+      paths = Dir["#{migrations_path}/*.rb"]
+      migrations = paths.map { |path|
+        Migration.from_path(path)
+      }
+      Migration::Collection.new(migrations)
+    end
+
+    def migrations_from_column_family
+      rows = column_family.select
+      migrations = rows.map { |row| Migration.from_hash(row) }
+      Migration::Collection.new(migrations)
+    end
+
+    # Private: The column family storing all
+    # migration information.
+    def column_family
+      @column_family ||= keyspace.column_family({
+        name: :migrations,
+        schema: {
+          primary_key: [:version, :name],
+          columns: {
+            version: :text,
+            name: :text,
+            migrated_at: :timestamp,
+          },
+        },
+      })
+    end
+
+    # Private: Ensures that the column family exists.
+    def ensure_column_family_exists
+      column_family.create unless column_family.exists?
+    end
+  end
+end
---
+++ spec/integration/cassanity/migrator_spec.rb
@@ -0,0 +1,104 @@
+require 'helper'
+require 'cassanity/migrator'
+
+describe Cassanity::Migrator do
+  let(:client) { Cassanity::Client.new(CassanityServers) }
+  let(:driver) { client.driver }
+  let(:keyspace) { client[:cassanity_test] }
+  let(:column_family) { subject.column_family }
+
+  let(:migrations_path) {
+    Pathname(__FILE__).dirname.join('fixtures', 'migrations')
+  }
+
+  subject {
+    described_class.new(keyspace, migrations_path)
+  }
+
+  before do
+    driver_drop_keyspace(driver, keyspace.name)
+    driver_create_keyspace(driver, keyspace.name)
+  end
+
+  describe "#migrate" do
+    context "when no migrations have been run" do
+      before do
+        subject.migrate
+      end
+
+      it "runs all migrations" do
+        rows = column_family.select
+        rows.size.should be(3)
+
+        create_users_row = rows.detect { |row|
+          row['name'] == 'create_users'
+        }
+        create_users_row.should_not be_nil
+        create_users_row['version'].should eq('20130224135000')
+
+        create_apps = rows.detect { |row|
+          row['name'] == 'create_apps'
+        }
+        create_apps.should_not be_nil
+        create_apps['version'].should eq('20130225135002')
+
+        add_username_to_users = rows.detect { |row|
+          row['name'] == 'add_username_to_users'
+        }
+        add_username_to_users.should_not be_nil
+        add_username_to_users['version'].should eq('20130226135004')
+      end
+    end
+
+    context "when some migrations have been run" do
+      it "runs migrations that need to be run" do
+
+      end
+    end
+  end
+
+  describe "#ensure_column_family_exists" do
+    context "when column family does not exist" do
+      before do
+        subject.ensure_column_family_exists
+      end
+
+      it "creates column family" do
+        names = keyspace.column_families.map(&:name)
+        string_names = names.map(&:to_s)
+        string_names.should include('migrations')
+      end
+    end
+
+    context "when column family does exist" do
+      let(:version) { '1' }
+
+      before do
+        # ensure column family exists
+        subject.ensure_column_family_exists
+
+        # pretend some migrations exist
+        subject.column_family.insert({
+          data: {
+            version: version,
+            name: 'create_users',
+            migrated_at: Time.now,
+          },
+        })
+
+        # ensure column family exists again
+        subject.ensure_column_family_exists
+      end
+
+      it "does nothing" do
+        rows = subject.column_family.select
+        rows.size.should be(1)
+
+        row = rows[0]
+        row['version'].should eq(version)
+        migrated_at = row['migrated_at'].to_i
+        migrated_at.should be_within(2).of(Time.now.to_i)
+      end
+    end
+  end
+end
