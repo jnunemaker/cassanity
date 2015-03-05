06df69271bcf8edb0ae2b653c1a3b04cf7d13ffe
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2015-03-05 14:58:19 +0000

    added config and specs

---
+++ lib/cassanity/config.rb
@@ -0,0 +1,20 @@
+
+require 'ostruct'
+require 'yaml'
+
+module Cassanity
+  class Config < OpenStruct
+    include Singleton
+
+    CONFIG_FILE = 'config/cassanity.yml'
+
+    def initialize
+      super YAML::load_file(CONFIG_FILE)[environment]
+    end
+
+    def environment
+      ENV['CASSANITY_ENV'] || 'development'
+    end
+
+  end
+end
---
+++ spec/unit/cassanity/config_spec.rb
@@ -0,0 +1,31 @@
+require 'helper'
+require 'cassanity/config'
+
+describe Cassanity::Config do
+
+  before do
+    stub_const 'Cassanity::Config::CONFIG_FILE', 'spec/support/cassanity.yml'
+  end
+
+  let(:config) { Class.new(Cassanity::Config).instance }
+
+  it 'successfully reads hosts config' do
+    config.hosts.should eq ['127.0.0.1']
+  end
+
+  it 'successfully reads port config' do
+    config.port.should eq 9042
+  end
+
+  { development: '_dev', test: '_test', production: '' }.each do |env, suffix|
+    it "successfully reads #{env} keyspace config" do
+      ENV['CASSANITY_ENV'] = env.to_s
+      config.keyspace.should eq "cassanity#{suffix}"
+    end
+  end
+
+  it 'successfully reads migrations path' do
+    config.migrations_path.should eq 'lib/db/migrations'
+  end
+
+end
