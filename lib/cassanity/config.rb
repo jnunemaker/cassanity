aafe559272b32e548d99b08099f8c17fb06c7aa9
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2015-10-06 12:07:52 +0100

    from OpenStruct to explicit methods for readability and discoverability

--- lib/cassanity/config.rb
+++ lib/cassanity/config.rb
@@ -5,18 +5,33 @@ require 'singleton'
 require 'erb'
 
 module Cassanity
-  class Config < OpenStruct
+  class Config
     include Singleton
 
     CONFIG_FILE = 'config/cassanity.erb.yml'
 
     def initialize
-      super YAML::load(ERB.new(File.read(CONFIG_FILE)).result)[environment]
+      @table = YAML::load(ERB.new(File.read(CONFIG_FILE)).result)[environment]
     end
 
     def environment
       ENV['CASSANITY_ENV'] || 'development'
     end
 
+    def hosts
+      @table[:hosts]
+    end
+
+    def port
+      @table[:port]
+    end
+
+    def migrations_path
+      @table[:migrations_path]
+    end
+
+    def keyspace
+      @table[:keyspace]
+    end
   end
 end
